5c71c4d99f0b0942d760b4085c05c0498c672f41
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-20 11:16:40 -0500

    Mods 1

--- lib/ui/pages/init_page.dart
+++ lib/ui/pages/init_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:menuapp/ui/general/colors.dart';
+import 'package:menuapp/utils/assets_data.dart';
 
 class InitPage extends StatefulWidget {
   const InitPage({Key? key}) : super(key: key);
@@ -11,26 +14,54 @@ class _InitPageState extends State<InitPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add,),
-            label: "Inicio"
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 8.0),
+        decoration: BoxDecoration(
+          color: kBrandSecondaryColor,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(28.0),
+            topRight: Radius.circular(28.0),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add,),
-            label: "Ordenes"
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          unselectedLabelStyle: TextStyle(
+            color: Colors.white70,
           ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.add,),
-              label: "Favoritos"
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.add,),
-              label: "Mi perfil"
-          ),
-        ],
+          unselectedItemColor: Colors.white70,
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AssetData.iconHome,
+                color: Colors.white,
+              ),
+              label: "Inicio",
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AssetData.iconCart,
+                color: Colors.white,
+              ),
+              label: "Ordenes",
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AssetData.iconFavorite,
+                color: Colors.white,
+              ),
+              label: "Favoritos",
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AssetData.iconProfile,
+                color: Colors.white,
+              ),
+              label: "Mi perfil",
+            ),
+          ],
+        ),
       ),
     );
   }
