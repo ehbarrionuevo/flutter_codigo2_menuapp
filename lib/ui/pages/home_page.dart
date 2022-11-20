80686e623cb4bbe942e86b5e4019165cb64a52c6
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-20 12:06:33 -0500

    Spacing

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -35,12 +35,7 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 
-                H1(text: "Hola a todos",),
-                H2(text: "Hola a todos",),
-                H3(text: "Hola a todos",),
-                H4(text: "Hola a todos",),
-                H5(text: "Hola a todos",),
-                H6(text: "Hola a todos",),
+
 
               ],
             ),
