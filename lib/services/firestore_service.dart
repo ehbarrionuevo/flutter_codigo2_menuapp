868382a560a1a322d66f6e2b2736cc29bbe0ebc0
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-25 20:15:51 -0500

    Model 2

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,17 +1,22 @@
 
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:menuapp/models/category_model.dart';
 
 class MyFirestoreService{
 
   final CollectionReference _reference = FirebaseFirestore.instance.collection("categories");
 
-  getCategories() async{
+  Future<List<CategoryModel>> getCategories() async{
     QuerySnapshot collection = await _reference.get();
     List<QueryDocumentSnapshot> docs = collection.docs;
+    List<CategoryModel> categories = [];
     for(var item in docs){
-      print(item);
+      CategoryModel model = CategoryModel.fromJson(item.data() as Map<String, dynamic>);
+      model.id = item.id;
+      categories.add(model);
     }
+    return categories;
   }
 
 }
