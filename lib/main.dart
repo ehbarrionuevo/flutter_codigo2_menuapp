9a32b331f0e9e0ad30faf1b3700b077c16260868
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-25 21:19:19 -0500

    Model 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:menuapp/providers/category_provider.dart';
 import 'package:menuapp/ui/pages/init_page.dart';
+import 'package:provider/provider.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,12 +17,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "MenuApp",
-      theme: ThemeData(
-        textTheme: GoogleFonts.openSansTextTheme(),
-      ),
-      home: InitPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (BuildContext context)=> CategoryProvider()),
+      ],
+      child: MaterialApp(
+        title: "MenuApp",
+        theme: ThemeData(
+          textTheme: GoogleFonts.openSansTextTheme(),
+        ),
+        home: InitPage(),
+      ),,
     );
   }
 }
