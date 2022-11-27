79c55b28aa3964a931df0b096bd85a81366695c2
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-27 10:10:43 -0500

    Provider 3.8

--- lib/ui/pages/order_page.dart
+++ lib/ui/pages/order_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:menuapp/models/product_order_model.dart';
 import 'package:menuapp/providers/order_provider.dart';
+import 'package:menuapp/ui/general/colors.dart';
 import 'package:menuapp/ui/widgets/general_widget.dart';
 import 'package:menuapp/ui/widgets/text_custom_widget.dart';
 import 'package:provider/provider.dart';
@@ -20,6 +21,7 @@ class OrderPage extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 H1(
                   text: "Mis Ordenes",
@@ -43,9 +45,26 @@ class OrderPage extends StatelessWidget {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              H5(text: e.title,),
+                              H5(
+                                text: e.title,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              spacing3,
+                              H6(text: "Cant. ${e.quantity}"),
+                              spacing3,
+                              InkWell(
+                                onTap: (){
+                                  orderProvider.removeProduct(e);
+                                },
+                                child: H6(text: "Eliminar"),
+                              ),
                             ],
                           ),
+                        ),
+                        H5(
+                          text: "S/ ${e.price.toStringAsFixed(2)}",
+                          fontWeight: FontWeight.w600,
+                          color: kBrandSecondaryColor.withOpacity(0.75),
                         ),
                       ],
                     ),
