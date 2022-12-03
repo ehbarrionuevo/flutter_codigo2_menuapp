727284770be3d3a7227da8f84bf58420bc69cd60
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-12-03 10:12:07 -0500

    Dashboard 1.3

--- lib/ui/pages/admin/dashboard_page.dart
+++ lib/ui/pages/admin/dashboard_page.dart
@@ -1,15 +1,25 @@
-
 import 'package:flutter/material.dart';
 import 'package:menuapp/ui/general/colors.dart';
 import 'package:menuapp/ui/widgets/text_custom_widget.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class DashboardPage extends StatelessWidget {
-
-  List<PieChartModel> dataPie =[
-    PieChartModel(x: "Juan",y: 20, color: Colors.indigo,),
-    PieChartModel(x: "Daniel",y: 30, color: Colors.greenAccent,),
-    PieChartModel(x: "Julio",y: 50, color: Colors.redAccent,),
+  List<PieChartModel> dataPie = [
+    PieChartModel(
+      x: "Juan",
+      y: 20,
+      color: Colors.indigo,
+    ),
+    PieChartModel(
+      x: "Daniel",
+      y: 30,
+      color: Colors.greenAccent,
+    ),
+    PieChartModel(
+      x: "Julio",
+      y: 50,
+      color: Colors.redAccent,
+    ),
   ];
 
   @override
@@ -30,20 +40,39 @@ class DashboardPage extends StatelessWidget {
                   color: kBrandSecondaryColor.withOpacity(0.60),
                 ),
                 SfCircularChart(
-                  title: ChartTitle(text: "Resumen mensual",),
+                  title: ChartTitle(
+                    text: "Resumen mensual",
+                  ),
                   legend: Legend(
-                    title: LegendTitle(text: "Detalle",),
+                    title: LegendTitle(
+                      text: "Detalle",
+                    ),
                     isVisible: true,
+                    legendItemBuilder: (String name, dynamic series,dynamic point, int index){
+                      return Container(
+                        margin: EdgeInsets.symmetric(vertical: 3.0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.ac_unit_outlined, size: 16),
+                            Text(" $name ${dataPie[index].y.toString()}"),
+                          ],
+                        ),
+                      );
+                    }
                   ),
                   tooltipBehavior: TooltipBehavior(
                     enable: true,
                   ),
                   series: <CircularSeries>[
-                    PieSeries<PieChartModel, String>(
+                    RadialBarSeries<PieChartModel, String>(
                       dataSource: dataPie,
-                      pointColorMapper: (model, index)=> model.color,
-                      xValueMapper: (model, index)=> model.x,
-                      yValueMapper: (model, index)=> model.y,
+                      pointColorMapper: (model, index) => model.color,
+                      xValueMapper: (model, index) => model.x,
+                      yValueMapper: (model, index) => model.y,
+                      radius: '90%',
+                      // explode: true,
+                      // explodeIndex: 1,
                     ),
                   ],
                 )
@@ -56,9 +85,14 @@ class DashboardPage extends StatelessWidget {
   }
 }
 
-class PieChartModel{
+class PieChartModel {
   final String x;
   final double y;
   final Color color;
-  PieChartModel({required this.x, required this.y, required this.color,});
+
+  PieChartModel({
+    required this.x,
+    required this.y,
+    required this.color,
+  });
 }
